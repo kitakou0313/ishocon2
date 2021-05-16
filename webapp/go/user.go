c427f7f8c1fb914cedb2abe2d17c3b6a38d4637f
kitakou0313 <kitakou0313@gmail.com>
2021-05-16 18:58:45 +0900

    Change where

--- webapp/go/user.go
+++ webapp/go/user.go
@@ -10,8 +10,8 @@ type User struct {
 }
 
 func getUser(name string, address string, myNumber string) (user User, err error) {
-	row := db.QueryRow("SELECT * FROM users WHERE name = ? AND address = ? AND mynumber = ?",
-		name, address, myNumber)
+	row := db.QueryRow("SELECT * FROM users WHERE mynumber = ? AND name = ? AND address = ?",
+		myNumber, name, address)
 	err = row.Scan(&user.ID, &user.Name, &user.Address, &user.MyNumber, &user.Votes)
 	return
 }
